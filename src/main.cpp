f761f3bf25614dc377d89cf94c0dbda5c1c0951f
Feng <Fenghao.wu8@gmail.com>
2025-06-04 16:33:07 +0200

    Ajout de code test bouton

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
-// Caca
+
 #define PIN_DI 34
 #define PIN_GI 35
 #define PIN_GI 36
 #define PIN_GE 39
+#define Bouton_Vert 32
+#define Bouton_Blue 33
 
 void setup()
 {
   Serial.begin(115200);
 
-  pinMode(PIN_DI, INPUT_PULLDOWN);
-  pinMode(PIN_GI, INPUT_PULLDOWN);
-  pinMode(PIN_GI, INPUT_PULLDOWN);
-  pinMode(PIN_GE, INPUT_PULLDOWN);
+  pinMode(PIN_DI, INPUT);
+  pinMode(PIN_GI, INPUT);
+  pinMode(PIN_GI, INPUT);
+  pinMode(PIN_GE, INPUT);
+  pinMode(Bouton_Vert, INPUT);
+  pinMode(Bouton_Blue, INPUT);
 }
 
 void loop()
@@ -21,6 +25,8 @@ void loop()
   int val2 = analogRead(PIN_GI);
   int val3 = analogRead(PIN_GI);
   int val4 = analogRead(PIN_GE);
+  char boutonVert = digitalRead(Bouton_Vert);
+  char boutonBlue = digitalRead(Bouton_Blue);
 
   Serial.print("CNY70_1: ");
   Serial.print(val1);
@@ -32,7 +38,12 @@ void loop()
   Serial.print(val3);
 
   Serial.print(" | CNY70_4: ");
-  Serial.println(val4);
+  Serial.print(val4);
 
-  delay(10);
+  Serial.print(" | Bouton Vert: ");
+  Serial.print(boutonVert == LOW ? "Appuye" : "Relache");
+  Serial.print(" | Bouton Blue: ");
+  Serial.println(boutonBlue == LOW ? "Appuye" : "Relache");
+
+  delay(100);
 }
