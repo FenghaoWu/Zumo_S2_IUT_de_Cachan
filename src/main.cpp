f6776faccaa98e68af7d9888c1d0a0a0e6dfd2f6
Feng <Fenghao.wu8@gmail.com>
2025-06-16 15:09:51 +0200

    Début de la fusion des codes test

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,46 @@
 #include <Arduino.h>
 
+//Partie Initialisation des Pins
+
 #define PIN_DI 34
 #define PIN_GI 35
-#define PIN_GI 36
+#define PIN_DE 36
 #define PIN_GE 39
+#define PWM_1 5
+#define PWM_2 16
 #define Bouton_Vert 32
-#define Bouton_Blue 33
+#define Bouton_Bleu 33
+
+int PWM_FREQUENCY = 5000;
+int PWM_RESOLUTION = 8;
+int PWM_CHANNEL_1 = 0;
+int PWM_CHANNEL_2 = 1;
+
+//Partie Fonctionenment
 
 void setup()
 {
   Serial.begin(115200);
-
   pinMode(PIN_DI, INPUT);
   pinMode(PIN_GI, INPUT);
-  pinMode(PIN_GI, INPUT);
+  pinMode(PIN_DE, INPUT);
   pinMode(PIN_GE, INPUT);
   pinMode(Bouton_Vert, INPUT);
-  pinMode(Bouton_Blue, INPUT);
+  pinMode(Bouton_Bleu, INPUT);
+  ledcSetup(PWM_CHANNEL_1, PWM_FREQUENCY, PWM_RESOLUTION);
+  ledcSetup(PWM_CHANNEL_2, PWM_FREQUENCY, PWM_RESOLUTION);
+  ledcAttachPin(PWM_1, PWM_CHANNEL_1);
+  ledcAttachPin(PWM_2, PWM_CHANNEL_2);
 }
 
 void loop()
 {
   int val1 = analogRead(PIN_DI);
   int val2 = analogRead(PIN_GI);
-  int val3 = analogRead(PIN_GI);
+  int val3 = analogRead(PIN_DE);
   int val4 = analogRead(PIN_GE);
   char boutonVert = digitalRead(Bouton_Vert);
-  char boutonBlue = digitalRead(Bouton_Blue);
-
-  Serial.print("CNY70_1: ");
-  Serial.print(val1);
-
-  Serial.print(" | CNY70_2: ");
-  Serial.print(val2);
-
-  Serial.print(" | CNY70_3: ");
-  Serial.print(val3);
-
-  Serial.print(" | CNY70_4: ");
-  Serial.print(val4);
-
-  Serial.print(" | Bouton Vert: ");
-  Serial.print(boutonVert == LOW ? "Appuye" : "Relache");
-  Serial.print(" | Bouton Blue: ");
-  Serial.println(boutonBlue == LOW ? "Appuye" : "Relache");
-
-  delay(100);
+  char boutonBleu = digitalRead(Bouton_Bleu);
+  ledcWrite(PWM_CHANNEL_1, 255);
+  ledcWrite(PWM_CHANNEL_2, 255);
 }
