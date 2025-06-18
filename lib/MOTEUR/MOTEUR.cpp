b4f9ee28a27155a778bd3606b28e065e67b0b801
Feng <Fenghao.wu8@gmail.com>
2025-06-18 13:18:54 +0200

    TOR et PWM inversé dans Moteur.h + Compréhension de suivi de ligne avec asservissement de base

--- lib/MOTEUR/MOTEUR.cpp
+++ lib/MOTEUR/MOTEUR.cpp
@@ -1,12 +1,13 @@
 #include "MOTEUR.h"
 #include <Arduino.h>
 
-#define PWM_Gauche 5
-#define TOR_Gauche 18
-#define TOR_Droite 17
-#define PWM_Droite 16
-
-int resolution_moteur = 10;
+#define PWM_Gauche 18
+#define TOR_Gauche 5
+#define TOR_Droite 16
+#define PWM_Droite 17
+#define AVANCER true
+#define RECULER false
+int resolution_moteur = 10; //PWM qui varie de 0 à 1023
 const int PWM_CHANNEL_0_GAUCHE = 0;
 const int PWM_CHANNEL_1_DROITE = 1;
 
@@ -23,22 +24,21 @@ void setup_moteur_pwm(float frequence_pwm, float resolution_pwm)
 
 void moteur_droit(int pwm)
 {
-    // int complement_pwm;
-    // complement_pwm = pwm - (pow(2, resolution_moteur) - 1);
-    // complement_pwm = abs(complement_pwm);
-    // ledcWrite(PWM_CHANNEL_2_DROITE, complement_pwm);
+    if(pwm >1023)   {
+
+        pwm= 1023;
+    }
+    if(pwm <0)   {
+
+        pwm= 0;
+    }
 
     ledcWrite(PWM_CHANNEL_1_DROITE, pwm);
-    digitalWrite(TOR_Droite, HIGH);
+    digitalWrite(TOR_Droite, AVANCER);
 }
 
 void moteur_gauche(int pwm)
-{
-    // int complement_pwm;
-    // complement_pwm = pwm - (pow(2, resolution_moteur) - 1);
-    // complement_pwm = abs(complement_pwm); // Ensure complement_pwm is positive
-    // ledcWrite(PWM_CHANNEL_0_GAUCHE, complement_pwm);
-    
+{    
     ledcWrite(PWM_CHANNEL_0_GAUCHE, pwm);
-    digitalWrite(TOR_Gauche, HIGH);
+    digitalWrite(TOR_Gauche, AVANCER);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,18 @@
 #define PIN_GE 35
 #define PIN_DE 36
 #define PIN_DI 39
-#define SDA 21
-#define SCL 22
+// #define SDA 21
+// #define SCL 22
 
 #define PWM_Base 511
 #define PWM_Min 0
 
-int erreur, correction = 0;
-float Kp = 0.05;
+float erreur;
+float erreur_precedente = 0;
 
+float correction = 0;
+float Kp = 1.5;
+float Kd = 0;
 // FONCTIONNEMENT
 
 void setup()
@@ -30,7 +33,6 @@ void setup()
   moteur_gauche(PWM_Min);
 }
 
-
 void loop()
 {
   int DroiteInterieur = abs(analogRead(PIN_DI));
@@ -39,24 +41,31 @@ void loop()
   int GaucheExterieur = abs(analogRead(PIN_GE));
 
   erreur = GaucheInterieur - DroiteInterieur;
-  correction = Kp * erreur;
-
+  correction = Kp * erreur + Kd * (erreur - erreur_precedente);
+  erreur_precedente = erreur;
+  // if (GaucheExterieur > 3000 && DroiteExterieur > 3000)
+  // {
+  //   moteur_droit(0);
+  //   moteur_gauche(0);
+  // }
+  // else
+  // {
   moteur_droit(PWM_Base - correction);
   moteur_gauche(PWM_Base + correction);
-
-  Serial.print("Gauche Exterieur: ");
-  Serial.print(GaucheExterieur);
-  Serial.print(" | Gauche Interieur: ");
-  Serial.print(GaucheInterieur);
-  Serial.print(" | Droite Interieur: ");
-  Serial.print(DroiteInterieur);
-  Serial.print(" | Droite Exterieur: ");
-  Serial.print(DroiteExterieur);
-  Serial.print(" | Erreur: ");
-  Serial.print(erreur);
-  Serial.print(" | moteur droit: ");
-  Serial.print(511 + correction);
-  Serial.print(" | moteur gauche: ");
-  Serial.println(511 - correction);
-  delay(100);
+  // }
+  // Serial.print("Gauche Exterieur: ");
+  // Serial.print(GaucheExterieur);
+  // Serial.print(" | Gauche Interieur: ");
+  // Serial.print(GaucheInterieur);
+  // Serial.print(" | Droite Interieur: ");
+  // Serial.print(DroiteInterieur);
+  // Serial.print(" | Droite Exterieur: ");
+  // Serial.print(DroiteExterieur);
+  // Serial.print(" | Erreur: ");
+  // Serial.print(erreur);
+  // Serial.print(" | moteur droit: ");
+  // Serial.print(PWM_Base - correction);
+  // Serial.print(" | moteur gauche: ");
+  // Serial.println(PWM_Base + correction);
+  // delay(100);
 }
