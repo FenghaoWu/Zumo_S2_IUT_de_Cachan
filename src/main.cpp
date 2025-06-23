fc29541bf3478d67787261801c390946007fda8a
Feng <Fenghao.wu8@gmail.com>
2025-06-23 12:29:28 +0200

    Code complet SANS couleur V2

--- src/main.cpp
+++ src/main.cpp
@@ -20,20 +20,12 @@ rgb_lcd lcd;
 float erreur;
 float erreur_precedente = 0;
 float correction = 0;
-<<<<<<< HEAD
 float Kp = 1.5;
 float Kd = 3;
 int etat = 0;
 // int coeff_stop_moteur = 1;
 int seuil = 1000;
 unsigned long TempsAttente = 0;
-=======
-float Kp = 1.3;
-float Kd = 3;
-int etat = 0;
-int coeff_stop_moteur = 1;
-int seuil = 1000;
->>>>>>> 8876f3e4877fbecc12758fcbee6cce60e0226263
 
 // FONCTIONNEMENT
 
@@ -49,18 +41,9 @@ void setup()
   moteur_gauche(PWM_Min);
   pinMode(PIN_BP0, INPUT_PULLDOWN); // BP0 en entré
   pinMode(PIN_BP1, INPUT_PULLDOWN); // BP1 en entré
-<<<<<<< HEAD
   lcd.begin(16, 2);
 }
-=======
->>>>>>> 8876f3e4877fbecc12758fcbee6cce60e0226263
 
-  lcd.begin(16, 2);
-
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
-  delay(1000);
-}
 void loop()
 {
   int DroiteInterieur = abs(analogRead(PIN_DI));
@@ -70,10 +53,9 @@ void loop()
   int boutonvert = digitalRead(PIN_BP0);
   int boutonbleu = digitalRead(PIN_BP1);
 
-<<<<<<< HEAD
   switch (etat)
   {
-  case 0: // Phase attente de demarrage (Violet)
+  case 0: // Phase attente de demarrage
 
     lcd.clear();
     lcd.setRGB(180, 0, 255);
@@ -162,107 +144,3 @@ void loop()
     break;
   }
 }
-=======
-  erreur = GaucheInterieur - DroiteInterieur;
-  correction = Kp * erreur + Kd * (erreur - erreur_precedente);
-  erreur_precedente = erreur;
-
-  // Serial.print("\n DE:");
-  // Serial.print(DroiteExterieur);
-  // delay(500);
-  // Serial.print("\n GE:");
-  // Serial.println(GaucheExterieur);
-  // delay(500);
-  // Serial.print("\n DI:");
-  // Serial.print(DroiteInterieur);
-  // delay(500);
-  // Serial.print("\n GI:");
-  // Serial.println(GaucheInterieur);
-  // delay(500);
-
-  // Serial.print("\n BP0:");
-  // Serial.println(boutonvert);
-  // delay(500);
-  // Serial.print("\n BP1:");
-  // Serial.println(boutonbleu);
-  // delay(500);
-
-  // delay(100); // temps de cycle
-  switch (etat)
-  {
-  case 0:
-    // etat = 0;
-    // if (boutonvert == 1)
-    // {
-      etat = 1;
-    // }
-
-    lcd.setRGB(255, 255, 0);
-    lcd.setCursor(0, 1);
-    // print the number of seconds since reset:
-    lcd.print(" PRET");
-
-    lcd.clear();
-
-    break;
-
-  case 1:
-    // if (boutonbleu == 1)
-    // {
-    //   etat = 2;
-    // }
-    if ((GaucheExterieur < seuil) && (DroiteExterieur < seuil))
-    {
-      etat = 3;
-    }
-    break;
-
-  case 3:
-    // delay(5000);
-    coeff_stop_moteur = 0;
-    lcd.setRGB(255, 0, 0);
-    lcd.setCursor(0, 1);
-    // print the number of seconds since reset:
-    lcd.print(" ARRIVE");
-
-    lcd.clear();
-
-    // etat = 1;
-    break;
-  }
-
-  moteur_droit((PWM_Base - correction) * coeff_stop_moteur);
-  moteur_gauche((PWM_Base + correction) * coeff_stop_moteur);
-}
-
-// }
-// Serial.print("Gauche Exterieur: ");
-// Serial.print(GaucheExterieur);
-// Serial.print(" | Gauche Interieur: ");
-// Serial.print(GaucheInterieur);
-// Serial.print(" | Droite Interieur: ");
-// Serial.print(DroiteInterieur);
-// Serial.print(" | Droite Exterieur: ");
-// Serial.print(DroiteExterieur);
-// Serial.print(" | Erreur: ");
-// Serial.print(erreur);
-// Serial.print(" | moteur droit: ");
-// Serial.print(PWM_Base - correction);
-// Serial.print(" | moteur gauche: ");
-// Serial.println(PWM_Base + correction);
-// delay(100);                                 temps en milliseconde
-
-// pour arreter les moteurs pendant 5s si les 2 capteurs exterrieurs captes la ligne
-// if (PIN_GE && PIN_DE)
-// {
-// delay(5000);
-//}
-
-// if (GaucheExterieur > 3000 && DroiteExterieur > 3000)
-// {
-//   moteur_droit(0);
-//   moteur_gauche(0);
-// }
-// else
-// {
->>>>>>> 8876f3e4877fbecc12758fcbee6cce60e0226263
